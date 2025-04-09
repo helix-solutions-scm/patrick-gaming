6b4174a8c88bd6a973884b80d54d7138d6efa2f2
Sam Belin <sam.belin@veup.com>
2025-04-09 12:27:59 -0600

    chore: adding remote state to project

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+    backend "s3" {
+    bucket = "terraform-state-goobie"
+    key    = "state"
+    region = "us-west-2"
+  }
 }
 
 # Configure the AWS Provider
